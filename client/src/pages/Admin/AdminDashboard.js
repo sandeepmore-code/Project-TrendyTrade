d92354c18e693e6eb27747eebbbdafd51c6f5bb5
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-08-16 23:09:12 +0530

    updates

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Layout from "../../components/layout/layout";
+import AdminMenu from "../../components/layout/AdminMenu.js";
+import { useAuth } from "../../Context/auth";
 
 const AdminDashboard = () => {
+  const [auth]= useAuth()
   return (
     <Layout title={"Admin Dasboard"}>
-      <div className="container-fluid"></div>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <div className="card">
+              <h1>{auth?.user?.name}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
